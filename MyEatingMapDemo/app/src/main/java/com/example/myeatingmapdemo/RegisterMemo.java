0495541592bf8cc0c2b07018adf6e03a329a9308
주이식 <helios2sic@gmail.com>
2019-12-01 11:05:41 +0900

    파일정리 및 서버에 메모전송기능 구현

---
+++ MyEatingMapDemo/app/src/main/java/com/example/myeatingmapdemo/RegisterMemo.java
@@ -0,0 +1,27 @@
+package com.example.myeatingmapdemo;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RegisterMemo extends StringRequest {
+
+    final static private String URL = "http://jkey20.cafe24.com/UserMemo.php";
+  private Map<String, String> parameters;
+
+  public RegisterMemo(String userMemo, String userLat, String userLon, Response.Listener<String> listener){
+    super(Method.POST, URL, listener, null);
+    parameters = new HashMap<>();
+    parameters.put("userMemo", userMemo);
+    parameters.put("userLat", userLat);
+    parameters.put("userLon", userLon);
+  }
+
+  @Override
+  public Map<String, String> getParams(){
+    return parameters;
+  }
+}

a0f56c7b8d5d5543cdb8058f5238af73c403b69b
uhhyunjoo <alro92333@gmail.com>
2020-12-08 13:22:01 +0900

    deleted not used 변수

--- MyEatingMapDemo/app/src/main/java/com/example/myeatingmapdemo/FIndRestaurant/FindRestaurantMarkActivity.java
+++ MyEatingMapDemo/app/src/main/java/com/example/myeatingmapdemo/FIndRestaurant/FindRestaurantMarkActivity.java
@@ -51,10 +51,6 @@ public class FindRestaurantMarkActivity extends MarkActivity {
 
         markerImage = BitmapFactory.decodeResource(this.getResources(), R.drawable.markerblack); // 중간지점의 마커로 사용할 이미지 지정
 
-        LinearLayout linearLayoutTmap = (LinearLayout) findViewById(R.id.mapview);
-        tMapView = new TMapView(this);
-        linearLayoutTmap.addView(tMapView);
-
         addMarkerItemToTmapView(markerImage, tMapView, currentPlaceValues.getPlacePoint()); // 검색한 위치에 마커를 뜨게 하는 메소드
 
         setTextView(currentPlaceValues, addressTextView, nameTextView);
